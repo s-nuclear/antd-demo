ceb14b58379f429f3549f8289eba23d675b09b3e
s-nuclear <97287480@qq.com>
2019-09-03 17:05:30 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,11 @@ module.exports = {
     css: {
         loaderOptions: { // 向 CSS 相关的 loader 传递选项
             less: {
+                modifyVars: {
+                    'primary-color': '#1DA57A',
+                    'link-color': '#1DA57A',
+                    'border-radius-base': '2px',
+                },
                 javascriptEnabled: true
             }
         }
